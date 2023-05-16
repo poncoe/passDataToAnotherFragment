99d9f07f56b593158b5a248105e8cb428cf19b76
poncoe <poncoe4mac@gmail.com>
2023-05-16 07:56:11 +0700

    implement view model

--- app/src/main/java/id/poncoe/latihandatafragment/ui/FragmentKedua.kt
+++ app/src/main/java/id/poncoe/latihandatafragment/ui/FragmentKedua.kt
@@ -5,23 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
-import id.poncoe.latihandatafragment.MainViewModel
 import id.poncoe.latihandatafragment.databinding.FragmentKeduaBinding
 
 class FragmentKedua : Fragment() {
     private lateinit var binding: FragmentKeduaBinding
-    val args: FragmentKeduaArgs by navArgs()
-
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(requireActivity())[MainViewModel::class.java]
-    }
+    private val args: FragmentKeduaArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentKeduaBinding.inflate(layoutInflater, container, false)
         return binding.root
